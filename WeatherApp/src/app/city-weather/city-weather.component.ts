c16908e4bfa3ed18be19c57e63cf1a3708d394db
LucaR05 <luca.rohne@gmx.de>
2025-06-19 14:18:33 +0200

    added forecast

--- WeatherApp/src/app/city-weather/city-weather.component.ts
+++ WeatherApp/src/app/city-weather/city-weather.component.ts
@@ -24,6 +24,13 @@ export class CityWeatherComponent implements OnInit{
   protected wind_speed: any;
   protected hide_weather:boolean = false;
 
+  protected current_day: any[] = [];
+  protected first_day: any[] = [];
+  protected second_day: any[] = [];
+  protected third_day: any[] = [];
+  protected fourth_day: any[] = [];
+  protected fifth_day: any[] = [];
+
 
   constructor(protected weatherService: WeatherService, private route: ActivatedRoute) {
   }
@@ -32,6 +39,13 @@ export class CityWeatherComponent implements OnInit{
 
   ngOnInit() {
     this.route.params.subscribe(params => {
+
+      this.current_day = [];
+      this.first_day = [];
+      this.second_day = [];
+      this.third_day = [];
+      this.fourth_day = [];
+      this.fifth_day = [];
 
       const lat = Number(sessionStorage.getItem("lat"));
       const lon = Number(sessionStorage.getItem("lon"));
@@ -53,6 +67,33 @@ export class CityWeatherComponent implements OnInit{
           console.log(data);
         }
       });
+    this.weatherService.getForecastByCoordinates(lat, lon).subscribe({
+      next: (data: any) => {
+        for (let item of data.list){
+          let date = new Date();
+          let itemDate: Date = new Date(item.dt*1000)
+          if (this.checkForDay(0, itemDate, date)){
+            this.current_day = this.saveDate(this.current_day, itemDate, item);
+          }
+          else if(this.checkForDay(1, itemDate, date)){
+            this.first_day = this.saveDate(this.first_day, itemDate, item);
+            console.log(this.first_day)
+          }
+          else if(this.checkForDay(2, itemDate, date)){
+            this.second_day = this.saveDate(this.second_day, itemDate, item);
+          }
+          else if(this.checkForDay(3, itemDate, date)){
+            this.third_day = this.saveDate(this.third_day, itemDate, item);
+          }
+          else if(this.checkForDay(4, itemDate, date)){
+            this.fourth_day = this.saveDate(this.fourth_day, itemDate, item);
+          }
+          else if(this.checkForDay(5, itemDate, date)) {
+            this.fifth_day = this.saveDate(this.fifth_day, itemDate, item);
+          }
+        }
+      }
+    })
     });
   }
 
@@ -62,6 +103,29 @@ export class CityWeatherComponent implements OnInit{
     const finalDate = new Date((dt + timezone + localOffset)*1000);
     return finalDate.toLocaleString();
 
+  }
+
+  private checkForDay(day: number, itemDate: Date, date: Date){
+    let compareDate = new Date(date);
+    compareDate.setDate(date.getDate() + day);
+    return itemDate.toLocaleString().split(',')[0] === compareDate.toLocaleString().split(',')[0]
+  }
+
+  private saveDate(li: any, itemDate:Date, item:any){
+    return [...li, [this.getDayByInt(itemDate.getDay()), itemDate.toLocaleString().split(',')[1], Math.round(item.main.temp - 273.15), item.weather[0].icon]];
+  }
+
+  private getDayByInt(int: number): string {
+    switch (int) {
+      case 0: return 'Sonntag';
+      case 1: return 'Montag';
+      case 2: return 'Dienstag';
+      case 3: return 'Mittwoch';
+      case 4: return 'Donnerstag';
+      case 5: return 'Freitag';
+      case 6: return 'Samstag';
+      default: return 'Ungültiger Tag';
+    }
   }
 
   getDt(){
